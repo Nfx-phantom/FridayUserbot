d8bf1e2375653607455fc015a949fc2a1354824d
NfxPhantom <70168042+Nfx-phantom@users.noreply.github.com>
2020-08-25 00:55:45 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -7,21 +7,21 @@ from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://telegra.ph/file/4fca63b472e86ecb7ae3d.jpg"
-pm_caption = "`FRIDAY IS:` **ONLINE**\n\n"
+pm_caption = "`PHANTOM IS:` **ONLINE**\n\n"
 pm_caption += "**SYSTEM STATUS**\n"
 pm_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
 pm_caption += "`DATABASE STATUS:` **Functional**\n"
-pm_caption += "**Current Branch** : `master`\n"
-pm_caption += "**Friday OS** : `3.14`\n"
-pm_caption += "**Current Sat** : `StarkGangSat-2.25`\n"
-pm_caption += f"**My Boss** : {DEFAULTUSER} \n"
+pm_caption += "**Current Branch** : `Nfx`\n"
+pm_caption += "**Phantom OS** : `3.14`\n"
+pm_caption += "**Current Sat** : `NfxGangSet-2.25`\n"
+pm_caption += f"¥¥ Mh Owner ¥¥ : {DEFAULTUSER} \n"
 pm_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
-pm_caption += "**License** : [MIT Licence](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n"
-pm_caption += "Copyright : By [StarkGang@Github](GitHub.com/StarkGang)\n"
-pm_caption += " [Deploy FridayUserbot](https://telegra.ph/FRIDAY-06-15)"
+pm_caption += "**License** : [Github ID](https://github.com/Nfx-phantom)\n"
+pm_caption += "Copyright : By [NfxPhantomGang@github](NfxPhantomGang@github.com)\n"
+pm_caption += "**Msg Reply Waiting Time** : `Depends Up On Mh Master `\n"
 
 @borg.on(admin_cmd(pattern=r"alive"))
-async def friday(alive):
+async def phantom(alive):
     chat = await alive.get_chat()
     """ For .alive command, check if the bot is running.  """
     await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
@@ -29,7 +29,7 @@ async def friday(alive):
 
     
 @borg.on(admin_cmd(pattern=r"Alive", allow_sudo=True))
-async def friday(alive):
+async def phantom(alive):
     chat = await alive.get_chat()
     """ For .alive command, check if the bot is running.  """
     await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
