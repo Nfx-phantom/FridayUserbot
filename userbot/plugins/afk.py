900c836d26e6c371f21050fdaa320dc30433794d
NfxPhantom <70168042+Nfx-phantom@users.noreply.github.com>
2020-08-25 20:31:51 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -1,4 +1,4 @@
-"""AFK Plugin for Friday
+"""AFK Plugin for Phantom
 Syntax: .afk REASON"""
 import asyncio
 import datetime
@@ -24,7 +24,7 @@ async def set_not_afk(event):
         try:
             await borg.send_message(  # pylint:disable=E0602
                 Config.PLUGIN_CHANNEL,  # pylint:disable=E0602
-                "#AfkLogger My Boss Went Afk"
+                "#AfkLogger My Owner Got Offline"
             )
         except Exception as e:  # pylint:disable=C0103,W0703
             await borg.send_message(  # pylint:disable=E0602
@@ -61,7 +61,7 @@ async def _(event):
             afk_time = datetime.datetime.now()  # pylint:disable=E0602
         USER_AFK = f"yes: {reason}"  # pylint:disable=E0602
         if reason:
-            await event.edit(f"My Boss Is Going Afk ! And The Reason is {reason}")
+            await event.edit(f"My Master Is Going Offline Dont Tag  ! And The Reason is {reason}")
         else:
             await event.edit(f"My Boss is Going")
         await asyncio.sleep(5)
@@ -121,10 +121,10 @@ async def on_afk(event):
             else:
                 afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
-        message_to_reply = f"**My Boss is AFK** ! \n\n**Reason** : `{reason}` \n\n**Afk Since** : {afk_since}" + \
-            f"\n\n__Kindly Leave A Message__ ! \n`He Will Reply To You Soon !`" \
+        message_to_reply = f"**My Owner Is Offline** ! \n\n**Reason** : `{reason}` \n\n**Afk Since** : {afk_since}" + \
+            f"\n\n__Kindly Leave A Message__ ! \n`Soory To Say But Master Will Not Reply Soon !`" \
             if reason \
-            else f"**Hello, Boss Is AFK Right Now And May Be Forgot List Reason ! Any Way He Will Come Back Soon !**"
+            else f"**Hello, Boss Is AFK Right Now And May Be Forgot List Reason ! Dont Wait For My Master Its On Her's Mood When To Come Online And When Not To !**"
         msg = await event.reply(message_to_reply)
         await asyncio.sleep(5)
         if event.chat_id in last_afk_message:  # pylint:disable=E0602
